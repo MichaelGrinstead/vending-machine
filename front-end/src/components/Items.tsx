84818605da62109ed4f2aa7a5d26aa406f79639e
Michael Grinstead <michael.a.grinstead@gmail.com>
2023-02-04 16:21:27 +1000

    added Items component

---
+++ front-end/src/components/Items.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Items = () => {
+  return (
+    <div className='Vending-Items'>
+      <div className='Item'>Item 1</div>
+      <div className='Item'>Item 2</div>
+      <div className='Item'>Item 3</div>
+      <div className='Item'>Item 4</div>
+      <div className='Item'>Item 5</div>
+      <div className='Item'>Item 6</div>
+      <div className='Item'>Item 7</div>
+      <div className='Item'>Item 8</div>
+      <div className='Item'>Item 9</div>
+      <div className='Item'>Item 10</div>
+      <div className='Item'>Item 11</div>
+      <div className='Item'>Item 12</div>
+    </div>
+  )
+}
+
+export default Items
