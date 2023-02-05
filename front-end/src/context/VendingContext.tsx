cd780029858461002d6b2ce2513104b6a40e9030
Michael Grinstead <michael.a.grinstead@gmail.com>
2023-02-05 22:55:32 +1000

    updated to correct state variable

--- front-end/src/context/VendingContext.tsx
+++ front-end/src/context/VendingContext.tsx
@@ -1,21 +1,22 @@
 import React, {createContext, useState, ReactNode} from 'react'
 
 interface VendingContextInterface {
-    order : string
-    setOrder : React.Dispatch<React.SetStateAction<string>>
+    itemSelected : string
+    setItemSelected : React.Dispatch<React.SetStateAction<string>>
 }
 
 const VendingContext = createContext<VendingContextInterface>({} as VendingContextInterface)
 
 export const VendingProvider  = ({children} : {children : ReactNode}) => {
 
-    const [order, setOrder] = useState<string>("")
+    
+    const [itemSelected, setItemSelected] = useState<string>("")
 
   return(
     <VendingContext.Provider
     value={{
-        order,
-        setOrder
+        itemSelected, 
+        setItemSelected
       }}
       >
         {children}
