724b65527995b2d43c03f1fe82e3deaba4ded2e5
Michael Grinstead <michael.a.grinstead@gmail.com>
2023-02-05 22:55:49 +1000

    updated to the correct state variable

--- front-end/src/components/Items.tsx
+++ front-end/src/components/Items.tsx
@@ -3,105 +3,105 @@ import VendingContext from '../context/VendingContext'
 
 const Items = () => {
 
-    const {order, setOrder} = useContext(VendingContext)
+    const {itemSelected, setItemSelected} = useContext(VendingContext)
     
 
 
   return (
         <div className='Vending-Items'>
-        {order === "1"
+        {itemSelected === "1"
         ?
         <div className='ItemSelected'>Item 1</div>
         :
-        <div className='Item' onClick={() => setOrder("1")}>Item 1</div>
+        <div className='Item' onClick={() => setItemSelected("1")}>Item 1</div>
 
         }  
 
-        {order === "2"
+        {itemSelected === "2"
         ?
         <div className='ItemSelected'>Item 2</div>
         :
-        <div className='Item' onClick={() => setOrder("2")}>Item 2</div>
+        <div className='Item' onClick={() => setItemSelected("2")}>Item 2</div>
 
         }  
 
-        {order === "3"
+        {itemSelected === "3"
         ?
         <div className='ItemSelected'>Item 3</div>
         :
-        <div className='Item' onClick={() => setOrder("3")}>Item 3</div>
+        <div className='Item' onClick={() => setItemSelected("3")}>Item 3</div>
 
         } 
 
-        {order === "4"
+        {itemSelected === "4"
         ?
         <div className='ItemSelected'>Item 4</div>
         :
-        <div className='Item' onClick={() => setOrder("4")}>Item 4</div>
+        <div className='Item' onClick={() => setItemSelected("4")}>Item 4</div>
 
         } 
 
-        {order === "5"
+        {itemSelected === "5"
         ?
         <div className='ItemSelected'>Item 5</div>
         :
-        <div className='Item' onClick={() => setOrder("5")}>Item 5</div>
+        <div className='Item' onClick={() => setItemSelected("5")}>Item 5</div>
 
         } 
         
-        {order === "6"
+        {itemSelected === "6"
         ?
         <div className='ItemSelected'>Item 6</div>
         :
-        <div className='Item' onClick={() => setOrder("6")}>Item 6</div>
+        <div className='Item' onClick={() => setItemSelected("6")}>Item 6</div>
 
         } 
 
-        {order === "7"
+        {itemSelected === "7"
         ?
         <div className='ItemSelected'>Item 7</div>
         :
-        <div className='Item' onClick={() => setOrder("7")}>Item 7</div>
+        <div className='Item' onClick={() => setItemSelected("7")}>Item 7</div>
 
         } 
         
-        {order === "8"
+        {itemSelected === "8"
         ?
         <div className='ItemSelected'>Item 8</div>
         :
-        <div className='Item' onClick={() => setOrder("8")}>Item 8</div>
+        <div className='Item' onClick={() => setItemSelected("8")}>Item 8</div>
 
         } 
 
-        {order === "9"
+        {itemSelected === "9"
         ?
         <div className='ItemSelected'>Item 9</div>
         :
-        <div className='Item' onClick={() => setOrder("9")}>Item 9</div>
+        <div className='Item' onClick={() => setItemSelected("9")}>Item 9</div>
 
         } 
 
-        {order === "10"
+        {itemSelected === "10"
         ?
         <div className='ItemSelected'>Item 10</div>
         :
-        <div className='Item' onClick={() => setOrder("10")}>Item 10</div>
+        <div className='Item' onClick={() => setItemSelected("10")}>Item 10</div>
 
         } 
 
-        {order === "11"
+        {itemSelected === "11"
         ?
         <div className='ItemSelected'>Item 11</div>
         :
-        <div className='Item' onClick={() => setOrder("11")}>Item 11</div>
+        <div className='Item' onClick={() => setItemSelected("11")}>Item 11</div>
 
         } 
 
-        {order === "12"
+        {itemSelected === "12"
         ?
         <div className='ItemSelected'>Item 12</div>
         :
-        <div className='Item' onClick={() => setOrder("12")}>Item 12</div>
+        <div className='Item' onClick={() => setItemSelected("12")}>Item 12</div>
 
         } 
     </div>
