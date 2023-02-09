fc76002a9097301489f557bfb402912d665d187b
Michael Grinstead <michael.a.grinstead@gmail.com>
2023-02-09 23:22:33 +1000

    added React router

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
 import './App.css';
-import Interface from './components/Interface';
-import Items from './components/Items'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Landing from './pages/Landing';
+import Vending from './pages/Vending';
 import {VendingProvider} from './context/VendingContext'
 
 function App() {
   return (
-    <VendingProvider>
-      <div
-      style={{height: "100%", width: "100%"}}
-      >
-        <div className='Vending'>
-          <div className='Vending-Inner'>
-            <Items/>
-          </div>
-        </div>
-        <Interface/>
-
-      </div>
-    </VendingProvider>
+    <BrowserRouter>
+      <VendingProvider>
+        <Routes>
+          <Route path= "/" element= {<Landing/>}/>
+          <Route path= "Vending" element= {<Vending/>}/>
+        </Routes>
+      </VendingProvider>
+    </BrowserRouter>
     
   );
 }
