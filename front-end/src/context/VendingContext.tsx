ffb2992f3b13fe11d0a9096ff1278b76faed19ae
Michael Grinstead <michael.a.grinstead@gmail.com>
2023-02-09 23:20:52 +1000

    added enum to manage connection state

--- front-end/src/context/VendingContext.tsx
+++ front-end/src/context/VendingContext.tsx
@@ -1,22 +1,35 @@
 import React, {createContext, useState, ReactNode} from 'react'
 
+export const enum connectionState {
+  UNCONNECTED,
+  NO_WALLET,
+  CONNECTED
+}
+
 interface VendingContextInterface {
     currentItemSelected : string
     setCurrentItemSelected : React.Dispatch<React.SetStateAction<string>>
+    connectionStatus : connectionState
+    setConnectionStatus: React.Dispatch<React.SetStateAction<connectionState>>
 }
 
 const VendingContext = createContext<VendingContextInterface>({} as VendingContextInterface)
 
 export const VendingProvider  = ({children} : {children : ReactNode}) => {
 
-    
-    const [currentItemSelected, setCurrentItemSelected] = useState<string>("")
+  
+  
+  const [currentItemSelected, setCurrentItemSelected] = useState<string>("")
+
+  const [connectionStatus, setConnectionStatus] = useState<connectionState>(connectionState.UNCONNECTED)
 
   return(
     <VendingContext.Provider
     value={{
         currentItemSelected, 
-        setCurrentItemSelected
+        setCurrentItemSelected,
+        connectionStatus,
+        setConnectionStatus
       }}
       >
         {children}
