1d8732e18a083c9589e6bc3b3c67f283bff88ed9
Michael Grinstead <michael.a.grinstead@gmail.com>
2023-02-11 19:51:22 +1000

    added purchasedItems component

--- front-end/src/pages/Vending.tsx
+++ front-end/src/pages/Vending.tsx
@@ -1,16 +1,31 @@
+import {useState} from 'react'
 import Interface from '../components/Interface'
 import Items from '../components/Items'
+import PurchasedItems from '../components/PurchasedItems'
 
 const Vending = () => {
+
+  const [showItems, setShowItems] = useState<boolean>()
+
   return (
     <div
         style={{height: "100%", width: "100%"}}
         >
           <div className='Vending'>
+            <h3 
+            className='Show-Items'
+            onClick={() => setShowItems(!showItems)}
+            >Show Items</h3>
             <div className='Vending-Inner'>
               <Items/>
             </div>
           </div>
+          {showItems
+          ?
+          <PurchasedItems/>
+          :
+          <></>
+          }
           <Interface/>
 
         </div>
