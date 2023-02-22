d5f8f3b6d2076b8a34d493d8afd056f5bd8437c6
Michael Grinstead <michael.a.grinstead@gmail.com>
2023-02-22 20:22:33 +1000

    deployed to mumbai

--- front-end/src/ContractObjects.ts
+++ front-end/src/ContractObjects.ts
@@ -1,6 +1,6 @@
 import {ethers} from 'ethers'
 import {Contract} from 'ethers'
-import GLTokenABI from './ABIs/GLTokenABI'
+import VendingTokenABI from './ABIs/VendingTokenABI'
 import VendingABI from './ABIs/VendingABI'
 
 
@@ -8,8 +8,8 @@ declare var window: any
 
 ///LocalHost
 
-const VendingTokenAddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
-const VendingAddress = "0x0165878A594ca255338adfa4d48449f69242Eb8F"
+const VendingTokenAddress = "0xE7BAc446Ca699463a99A9F860331Cf28AC8b6183"
+const VendingAddress = "0xe7b905Cf4D172ba8E364916D07e74e1fF9a99034"
 
 
 
@@ -24,7 +24,7 @@ let VendingContract : Contract
 if(window.ethereum != null) {
     provider  = new ethers.providers.Web3Provider(window.ethereum)
     signer = provider.getSigner()
-    VendingTokenContract = new ethers.Contract(VendingTokenAddress, GLTokenABI, signer);
+    VendingTokenContract = new ethers.Contract(VendingTokenAddress, VendingTokenABI, signer);
     VendingContract = new ethers.Contract(VendingAddress, VendingABI, signer); 
   
 }   
