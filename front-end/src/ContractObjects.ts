79528ae90007dfd9bf8736561a5bd9c00467c75a
Michael Grinstead <michael.a.grinstead@gmail.com>
2023-03-17 23:18:07 +1000

    updated

--- front-end/src/ContractObjects.ts
+++ front-end/src/ContractObjects.ts
@@ -1,20 +1,20 @@
 import {ethers} from 'ethers'
 import {Contract} from 'ethers'
 import VendingTokenABI from './ABIs/VendingTokenABI'
-import VendingABI from './ABIs/VendingABI'
+import VendingFactoryABI from './ABIs/VendingFactoryABI'
 
 
 declare var window: any
 
 ///LocalHost
 
-// const VendingTokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-// const VendingAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+const VendingTokenAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+const VendingFactoryAddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
 
 //Mumbai
 
-const VendingTokenAddress = "0x961c835b6D4713F390a01B0358059C84c9b5773F"
-const VendingAddress = "0xd07C90b4dFd6CCD7AfaDAB0E1500AAF0A89EEeFb"
+// const VendingTokenAddress = "0x961c835b6D4713F390a01B0358059C84c9b5773F"
+// const VendingFactoryAddress = "0xd07C90b4dFd6CCD7AfaDAB0E1500AAF0A89EEeFb"
 
 
 
@@ -22,7 +22,7 @@ const VendingAddress = "0xd07C90b4dFd6CCD7AfaDAB0E1500AAF0A89EEeFb"
 let provider : ethers.providers.Web3Provider
 let signer : ethers.providers.JsonRpcSigner
 let VendingTokenContract : Contract
-let VendingContract : Contract 
+let VendingFactoryContract : Contract 
 
 
 
@@ -31,8 +31,8 @@ if(window.ethereum != null) {
     provider  = new ethers.providers.Web3Provider(window.ethereum)
     signer = provider.getSigner()
     VendingTokenContract = new ethers.Contract(VendingTokenAddress, VendingTokenABI, signer);
-    VendingContract = new ethers.Contract(VendingAddress, VendingABI, signer); 
+    VendingFactoryContract = new ethers.Contract(VendingFactoryAddress, VendingFactoryABI, signer); 
   
 }   
 
-export {provider, signer, VendingContract, VendingTokenContract, VendingAddress, VendingTokenAddress}
+export {provider, signer, VendingFactoryContract, VendingTokenContract, VendingTokenAddress}
