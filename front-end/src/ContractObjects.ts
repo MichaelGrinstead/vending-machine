6b30a76686e99cf75938cd8e57c23ca3f78c9ca5
Michael Grinstead <michael.a.grinstead@gmail.com>
2023-03-18 11:50:12 +1000

    deployed on mumbai testnet

--- front-end/src/ContractObjects.ts
+++ front-end/src/ContractObjects.ts
@@ -8,13 +8,13 @@ declare var window: any
 
 ///LocalHost
 
-const VendingTokenAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
-const VendingFactoryAddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
+// const VendingTokenAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+// const VendingFactoryAddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
 
-//Mumbai
+// Mumbai
 
-// const VendingTokenAddress = "0x961c835b6D4713F390a01B0358059C84c9b5773F"
-// const VendingFactoryAddress = "0xd07C90b4dFd6CCD7AfaDAB0E1500AAF0A89EEeFb"
+const VendingTokenAddress = "0x6Dc30e207Dd70fD7EAdF913a42C95B1aE7C868bA"
+const VendingFactoryAddress = "0xB3461553BE94C26668fd18ae412D2AA725D7e4b6"
 
 
 
